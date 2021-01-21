5f589cd35393a7567381eb32e7201d9028a272bd
김 태오 <teo@monitoring6.com>
2021-01-21 16:06:05 +0900

    pagesample

---
+++ NavigationExpamle/NavigationExpamle/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using NavigationExpamle.Models;
+using NavigationExpamle.ViewModels;
+using NavigationExpamle.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigationExpamle.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
